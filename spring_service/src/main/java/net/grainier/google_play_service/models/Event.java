44e843d985d262024113bc5e239168a3931d9812
Grainier Perera <grainierp@zone24x7.com>
2014-02-27 16:32:43 +0530

    scraper project added

---
+++ spring_service/src/main/java/net/grainier/google_play_service/models/Event.java
@@ -0,0 +1,55 @@
+package net.grainier.google_play_service.models;
+
+import net.grainier.google_play_service.util.enums.EventType;
+
+/**
+ * Created by grainierp on 1/22/14.
+ */
+public class Event {
+    private EventType type;
+    private long timeStamp;
+    private long count;
+
+    public Event(EventType type) {
+        this.type = type;
+        this.timeStamp = 0L;
+        this.count = 0L;
+    }
+
+    public Event() {
+        // to get rid of JsonMappingException
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+    @Override
+    public boolean equals(Object other){
+        if (other == null) return false;
+        if (other == this) return true;
+        if (!(other instanceof Event))return false;
+        Event event = (Event) other;
+        return this.getType() == event.getType();
+    }
+}
---
+++ spring_service/src/main/java/net/grainier/google_play_service/util/ResponseWrapper.java
@@ -0,0 +1,42 @@
+package net.grainier.google_play_service.util;
+
+import net.grainier.google_play_service.util.enums.ResponseStatusEnum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ResponseWrapper<T> {
+
+	public ResponseContainer<T> Wrap(List<T> ol,ResponseStatusEnum status,String message) {
+	
+		ResponseContainer<T> rc = new ResponseContainer<T>();
+		
+		try {
+			rc.setResData(ol);
+			rc.setResCode(status.toString());
+			rc.setResMessage(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return rc;
+	}
+
+    public ResponseContainer<T> WrapSingle(T obj,ResponseStatusEnum status,String message) {
+
+        ResponseContainer<T> rc = new ResponseContainer<T>();
+
+        try {
+            rc.setResData(new ArrayList<T>());
+            rc.getResData().add(obj);
+            rc.setResCode(status.toString());
+            rc.setResMessage(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return rc;
+    }
+	
+}
