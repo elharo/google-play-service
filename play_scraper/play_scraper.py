06c5e2bc6f8b3caac239fcdfcc8435d29dda156a
Grainier Perera <grainierp@zone24x7.com>
2014-03-03 17:13:03 +0530

    Worker Pool Added

--- play_scraper/play_scraper.py
+++ play_scraper/play_scraper.py
@@ -1,6 +1,7 @@
 from ApplicationScraper import ApplicationScraper
 import redis
 import pickle
+import workerpool
 
 __author__ = 'grainier'
 
@@ -23,3 +24,18 @@ for application_key in application_keys:
     pass
 
 print('done')
+
+def process_xls_sheets(path):
+    xls_files = get_xls_files(path)
+    xls_processed_data = get_xls_processed_data(xls_files)
+
+    # Make a pool, five threads
+    pool = workerpool.WorkerPool(size=5)
+
+    # Perform the mapping
+    pool.map(process_single_row, xls_processed_data)
+
+    # Send shutdown jobs to all threads, and wait until all the jobs have been completed
+    pool.shutdown()
+    pool.wait()
+    pass
